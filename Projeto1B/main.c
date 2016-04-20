fc52a8a0e3723464f76dbe7ff525a842689f8fbb
Artur Mafud <tuifm@hotmail.com>
2016-04-20 14:04:23 -0300

    Diversas mudanças e adiçoes

--- Projeto1B/main.c
+++ Projeto1B/main.c
@@ -3,12 +3,12 @@
 #include <math.h>
 #include <time.h>
 
+// pi = M_PI ; pi/2 = M_PI_2 ; pi/4 = M_PI_4 Constantes do Pi para se saber
 // Programa que calcula uma integral por 1/3 de Simpson. Aceita até 10 valores tabelados como entrada
 int main()
 {
-  float x[10],y[10],soma=0,h,temp;
-  int i,dados,j,k=0;
-  //float fact(int);
+  float x[10],y[10],soma=0,h,valorAnterior,integralFinal;
+  int i,dados,k=1,identificaFx=0;
 
   // Coisas necessárias para medir tempo de execução ******
      clock_t tempo;
@@ -20,43 +20,61 @@ int main()
 
           for(i = 0; i < dados; i++)
           {
-           printf("\nDigite o valor de x%d: ",i);
+           printf("\nDigite o valor de fi%d: ",i);
            scanf("%f",&x[i]);
-           printf("\nDigite o valor de f(x%d): ",i);
+           printf("\nDigite o valor de f(fi%d): ",i);
            scanf("%f",&y[i]);
           }
   //
 
-      h = x[1]-x[0]; // h = b - a (limites de integração)
-      dados = dados-1; // corrige o fato de que o PC computa a partir do 0 e o usuário acaba digitando 1 a mais (pensa a partir de 1)
-      soma = soma + y[0]; // ja seta o valor da soma da fórmula com f(x0)
+      /* Aqui é o local no código original  h = x[1]-x[0]; // h = b - a (limites de integração)
+      dados = dados - 1; // corrige o fato de que o PC computa a partir do 0 e o usuário acaba digitando 1 a mais (pensa a partir de 1)
+      soma = soma + y[0]; // ja seta o valor da soma da fórmula com f(x0)*/
+
+      do
+      {
+          h = (2 * M_PI) / ((pow(2,k)) * 3);
+          dados = dados - 1; // corrige o fato de que o PC computa a partir do 0 e o usuário acaba digitando 1 a mais (pensa a partir de 1)
+          soma = soma + y[0]; // ja seta o valor da soma da fórmula com f(x0)
 
           for(i = 1;i < dados;i++)
-          {
-                // caso ta nos elementos que são multiplicados por 4 na fórmula
-                if(k == 0)
-                {
-                 soma = soma + 4 * y[i];
-                 k = 1;
-                }
+              {
+                    // caso ta nos elementos que são multiplicados por 4 na fórmula
+                    if(identificaFx == 0)
+                    {
+                     soma = soma + 4 * y[i];
+                     identificaFx = 1;
+                    }
 
-                // caso os de 2
-                else
-                {
-                 soma = soma + 2 * y[i];
-                 k = 0;
-                }
-          }
+                    // caso os de 2
+                    else
+                    {
+                     soma = soma + 2 * y[i];
+                     identificaFx = 0;
+                    }
+              }
 
-       soma = soma + y[i]; // soma o f(xn)
-       soma = soma * (h/3); // termina a conta multiplicando pelo h/3
+           soma = soma + y[i]; // soma o f(xn)
+           soma = soma * (h/3); // termina a conta multiplicando pelo h/3
 
-    printf("\nResultado da integral:  I = %f  ",soma);
+        integralFinal = (1 / (2 * M_PI)) * soma;
+        printf("\nResultado da integral: %f  ",integralFinal);
+
+            if (k > 1)
+            {
+                printf("\nDiferenca I%d - I%d : %f",k,k-1,integralFinal - valorAnterior);
+            }
+
+        valorAnterior = integralFinal;
+        soma = 0;
+        k++;
+
+      }while(k <= 10);
 
     tempo = clock() - tempo;
     double tempoLevado = ((double) tempo) / CLOCKS_PER_SEC; // pega o tempo em segundos
 
-    printf("\nO calculo levou %f segundos para ser executado \n",tempoLevado);
+    printf("\n\nO calculo levou %f segundos para ser executado \n",tempoLevado);
 
     return 0;
 }
