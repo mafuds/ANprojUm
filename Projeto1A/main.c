4f3d2819f88ed8ccb23348c650ec36dba222fc0a
Artur Mafud <tuifm@hotmail.com>
2016-06-29 21:55:49 -0300

    Versão final
    Adicionando a versão que ficou como final. Tudo funcionando. Splines sendo produzidos

--- Projeto1A/main.c
+++ Projeto1A/main.c
@@ -3,149 +3,107 @@
 #include <math.h>
 #include <time.h>
 
-#define MALLOC(ptr,typ,num) {                                           \
-  (ptr) = (typ *)malloc((num) * sizeof(typ));                           \
-  if((ptr) == NULL){                                                    \
-    fprintf(stderr,"-E- %s line %d: Memory allocation failure.\n",      \
-    __FILE__,__LINE__);                                                 \
-    exit(EXIT_FAILURE);                                                 \
-  }                                                                     \
-}
-
-/* http://oceancolor.gsfc.nasa.gov/staff/norman/seawifs_image_cookbook/faux_shuttle/spline.c
-    http://apps.nrbook.com/c/index.html */
-
-// Função que calcula todo o polinômio spline de uma vez
-void spline(float x[],float y[], int n, float yp1, float ypn, float	y2[]);
-
-// Função a ser chamada para que se obtenha o valor
-void splint(float xa[], float ya[], float y2a[], int n, float x, float *y);
+#define DOISPI (2 * M_PI)
+#define CONST 1 / DOISPI
+#define MENOSPI -M_PI
+/// ( 1 / sqrt(2.01 - 2 * cos x) - cos x)
+/// com h variavel de = 2 * pi / 2^k * 3
 
 int main()
 {
-    float x[10],y[10];
-    int i,dados;
+    int i, j, numParticoes, contPos, contNeg, opcao, loop;
+    double *a, primDerivadax0, primDerivadaxn, *vetB, *vetDeX, passoH, *vetL, *vetMi, *vetZ, *b, *c, *d;
 
-      // Coisas necessárias para medir tempo de execução ******
-         clock_t tempo;
-         tempo = clock();
+    printf("\t\t\tEX 01 - PROJETO 1\n\n\t\t\t  SPLINE FIXADO\n\n");
+    printf("1. Derivadas em +-pi\n2. Aproximacoes das derivadas com formula de 3 pontos em +-pi\n\n\t");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
+    printf("\n\nQuantas iteracoes (1 | 2 | 3 | 4 | 5): ");
+    scanf("%d", &loop);
 
-      // Pega os valores da tabela pra calcular
-          printf("\nQuantos dados serao inseridos? ");
-          scanf("%d",&dados);
+    for (i = 1; i <= loop; i++)
+    {
+        numParticoes = pow(2, i) * 3;
+        passoH = DOISPI / numParticoes;
 
-              for(i = 0; i < dados; i++)
-              {
-               printf("\nDigite o valor de fi%d: ",i);
-               scanf("%f",&x[i]);
-               printf("\nDigite o valor de f(fi%d): ",i);
-               scanf("%f",&y[i]);
-              }
-      //
+        a = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        vetDeX = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        vetDeX[0] = MENOSPI;
 
-    // Coisas necessárias para medir tempo de execução ******
-     clock_t tempo;
-     tempo = clock();
-
-     tempo = clock() - tempo;
-    double tempoLevado = ((double) tempo) / CLOCKS_PER_SEC; // pega o tempo em segundos
-
-    printf("\n\nO calculo levou %f segundos para ser executado \n",tempoLevado);
-    return 0;
-}
-
-void spline(float x[],float y[], int n, float yp1, float ypn, float	y2[])
-{
-    int	i,k;
-      float	p,qn,sig,un,*u;
-
-      MALLOC(u,float,n-1);
-
-          if(yp1 > 0.99e30)
-            y2[0] = u[0] = 0.0;
-
-          else
-          {
-            y2[0] = -0.5;
-            u[0] = (3.0/(x[1]-x[0]))*((y[1]-y[0])/(x[1]-x[0])-yp1);
-          }
-
-          for(i = 1; i < n-1; i++)
-          {
-            sig = (x[i] - x[i-1])/(x[i+1] - x[i-1]);
-            p = sig*y2[i-1] + 2.0;
-            y2[i] = (sig - 1.0)/p;
-            u[i] = (y[i+1] - y[i])/(x[i+1] - x[i]) - (y[i] - y[i-1])/(x[i] - x[i-1]);
-            u[i] = (6.0*u[i]/(x[i+1] - x[i-1]) - sig*u[i-1])/p;
-          }
-
-      if(ypn > 0.99e30)
-        qn = un = 0.0;
-
-      else
-      {
-        qn = 0.5;
-        un = (3.0/(x[n-1] - x[n-2]))*(ypn - (y[n-1] - y[n-2])/(x[n-1] - x[n-2]));
-      }
-
-      y2[n-1] = (un - qn*u[n-2])/(qn*y2[n-2] + 1.0);
-
-      for(k = n-2; k >= 0; k--)
-      {
-        y2[k] = y2[k]*y2[k+1] + u[k];
-      }
-
-      free(u);
-}
-
-void splint(float xa[], float ya[], float y2a[], int n, float x, float *y)
-{
-    int		klo,khi,k;
-    float		h,b,a;
-    static int	pklo=0,pkhi=1;
-
-      /*
-      Based on the assumption that sequential calls to this function are made
-      with closely-spaced, steadily-increasing values of x, I first try using
-      the same values of klo and khi as were used in the previous invocation.
-      If that interval is no longer correct, I do a binary search for the
-      correct interval.
-      */
-
-      if(xa[pklo] <= x && xa[pkhi] > x)
-      {
-        klo = pklo;
-        khi = pkhi;
-      }
-
-      else
-      {
-        klo = 0;
-        khi = n - 1;
-
-            while(khi - klo > 1)
-            {
-              k = (khi + klo) >> 1;
-
-                  if(xa[k] > x)
-                    khi = k;
-                  else
-                    klo = k;
+        /// Obtendo os y's e x's da função
+        for (j = 0; j <= numParticoes; j++)
+        {
+            a[j] = 1 / sqrt(2.01 - (2 * cos(vetDeX[j]))) - cos(vetDeX[j]);
+            if (j != numParticoes)
+                vetDeX[j+1] = vetDeX[j] + passoH;
+            if (j > 0) {
+            	if (vetDeX[j] >= acos(1.01/2) && vetDeX[j-1] <= acos(1.01/2))
+        			contPos = j - 1;
+        		if (vetDeX[j] >= -acos(1.01/2) && vetDeX[j-1] <= -acos(1.01/2))
+        			contNeg = j - 1;
             }
+        }
 
-        pklo = klo;
-        pkhi = khi;
-      }
+        // melhor essa formula por causa das extremidades, pois as info sobre f fora do intervalo não estão dispo
+        // o erro é maior do que da outra equação mas tem esse lance aí de cima
+        if (opcao == 1) {
+            primDerivadax0 = 0;
+            primDerivadaxn = 0;
+        }
+        else {
+            primDerivadax0 = 1 / (2 * passoH) * (-3 * a[0] + 4 * a[1] - a[2]);
+            primDerivadaxn = 1 / (2 * passoH) * (a[numParticoes - 2] - 4 * a[numParticoes - 1] + 3 * a[numParticoes]);
+        }
 
-      h = xa[khi] - xa[klo];
+        /// Criação do vetor b (Ac = b)
+        vetB = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        vetB[0] = 3 * (a[1] - a[0]) / passoH - 3 * primDerivadax0;
+        vetB[numParticoes] = 3 * primDerivadaxn - 3 * (a[numParticoes] - a[numParticoes - 1]) / passoH;
 
-      if(h == 0)
-      {
-        fprintf(stderr,"-E- %s line %d: Bad xa input to function splint()\n", __FILE__,__LINE__);
-        exit(EXIT_FAILURE);
-      }
+        for (j = 1; j <= numParticoes - 1; j++)
+            vetB[j] = (3 / passoH) * (a[j+1] - a[j]) - (3 / passoH) * (a[j] - a[j-1]);
 
-      a = (xa[khi] - x)/h;
-      b = (x - xa[klo])/h;
-      *y = a*ya[klo] + b*ya[khi] + ((a*a*a - a)*y2a[klo] + (b*b*b - b)*y2a[khi])*(h*h)/6.0;
+        /// Solução para o sistema linear
+        vetL = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        vetMi = (double *) malloc ((numParticoes) * sizeof(double));
+        vetZ = (double *) malloc ((numParticoes + 1) * sizeof(double));
+
+        vetL[0] = 2 * passoH;
+        vetMi[0] = 0.5;
+        vetZ[0] = vetB[0] / vetL[0];
+
+        for (j = 1; j <= numParticoes - 1; j++)
+        {
+            vetL[j] = 2 * (vetDeX[j+1] - vetDeX[j-1]) - passoH * vetMi[j-1];
+            vetMi[j] = passoH / vetL[j];
+            vetZ[j] = (vetB[j] - passoH * vetZ[j-1]) / vetL[j];
+        }
+
+        vetL[numParticoes] = passoH * (2 - vetMi[numParticoes-1]);
+        vetZ[numParticoes] = (vetB[numParticoes] - passoH * vetZ[numParticoes-1]) / vetL[numParticoes];
+        c = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        c[numParticoes] = vetZ[numParticoes];
+        b = (double *) malloc ((numParticoes + 1) * sizeof(double));
+        d = (double *) malloc ((numParticoes + 1) * sizeof(double));
+
+        for (j = numParticoes - 1; j >= 0; j--)
+        {
+        	c[j] = vetZ[j] - vetMi[j] * c[j+1];
+        	b[j] = 	(a[j+1] - a[j]) / passoH - passoH * (c[j+1] + 2 * c[j]) / 3;
+        	d[j] = (c[j+1] - c[j]) / (3 * passoH);
+        }
+
+        printf("\n\nK = %d | Num de Pontos: %d\n", i, numParticoes + 1);
+        for (j = 0; j < numParticoes; j++)
+            printf("\nS%d(x): %.3f + %.3f(x - %.3f) + %.3f(x - %.3f)² + %.3ff(x - %.3f)³", j, a[j], b[j], vetDeX[j], c[j], vetDeX[j], d[j], vetDeX[j]);
+
+        printf("\n\nYmin da funcao em +-arccos(1.01/2) -> +-1.04141... = 0.495");
+       	double r = a[contPos] + b[contPos] * (acos(1.01/2) - vetDeX[contPos]) + c[contPos] * pow((acos(1.01/2) - vetDeX[contPos]), 2) + d[contPos] * pow((acos(1.01/2) - vetDeX[contPos]), 3);
+		printf("\n\nS%d(1.04141): %.5f", contPos, r);
+		r = a[contNeg] + b[contNeg] * (-acos(1.01/2) - vetDeX[contNeg]) + c[contNeg] * pow((-acos(1.01/2) - vetDeX[contNeg]), 2) + d[contNeg] * pow((-acos(1.01/2) - vetDeX[contNeg]), 3);
+		printf("\nS%d(-1.04141): %.5f", contNeg, r);	
+
+    }
+
+    return 0;
 }
